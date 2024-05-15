ad9ad3bd40faaa099d98a3a94c68eeb77801788d
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2024-05-15 14:22:53 -0400

    Initial Commit

---
+++ Simplify Golf/Simplify_GolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simplify_GolfApp.swift
+//  Simplify Golf
+//
+//  Created by Jayson Dasher on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Simplify_GolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
