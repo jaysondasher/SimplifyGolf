f853eed76798853909468454aa36128df550b3b3
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2024-05-15 14:38:13 -0400

    Initial Commit

---
+++ Simplify Golf Watch Watch App/Simplify_Golf_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simplify_Golf_WatchApp.swift
+//  Simplify Golf Watch Watch App
+//
+//  Created by Jayson Dasher on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Simplify_Golf_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
