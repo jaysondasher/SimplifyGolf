e8f52ba0f1346a6fffdc496462f8292ad04d6fc5
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2024-07-24 23:00:34 -0400

    Coming soon message for watch app

--- Simplify Golf Watch Watch App/ContentView.swift
+++ Simplify Golf Watch Watch App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, watch!")
+            Text("Watch App Coming Soon!")
         }
         .padding()
     }
